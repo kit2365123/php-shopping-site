f83b4672804742c2f789d5a3daa6b14c468c0b7f
Alvin Leung <alvinleung198123@gmail.com>
2021-07-08 22:25:33 +0800

    get gem data from database and display the data to the webpage

--- index.php
+++ index.php
@@ -5,17 +5,29 @@
 
     <?php
 
-    foreach($items as $key => $item) {
-        echo '<img src="./images/'
-        .$item['image'].
-        '" /><p>'
-        .$item['name'].
-        '<br>$'
-        .$item['price'].
-        '<br> <a href="./order.php?item_id='
-        .$item['item_id'].
-        '"class="buyBtn">Buy</a><br></p>';
+    // display items
+    $gemQ = mysqli_query($dbConnection, "SELECT * FROM `gem`");
+    while ($gem = mysqli_fetch_assoc($gemQ)) {
+        echo '<div>
+        <img src="./images/'.$gem['image'].'" />
+        <p>
+        Name: '.$gem['name'].'<br>
+        Price: $'.$gem['price'].'<br>
+        <a href="./order.php?gem_id='.$gem['gem_id'].'" class="buyBtn">Order'.$gem['name'].'</a><br>
+        </div>';
     }
+
+    // foreach($items as $key => $item) {
+    //     echo '<img src="./images/'
+    //     .$item['image'].
+    //     '" /><p>'
+    //     .$item['name'].
+    //     '<br>$'
+    //     .$item['price'].
+    //     '<br> <a href="./order.php?item_id='
+    //     .$item['item_id'].
+    //     '"class="buyBtn">Buy</a><br></p>';
+    // }
 
     ?>
 
